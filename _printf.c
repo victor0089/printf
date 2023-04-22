3626384d7185791ff95264733e3b4c3f8f1c03b0
Victor <victor.mecdoors@gmail.com>
2023-04-22 12:28:59 -0700

    rebulding

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int flgs, wid, prc, sz, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -32,13 +32,13 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flgs = get_flags(format, &i);
+			wid = get_width(format, &i, list);
+			prc = get_precision(format, &i, list);
+			sz = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+				flgs, wid, prc, sz);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
