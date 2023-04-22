b277a2524bfa04e4e96047aeaaaf1ee8b32adf32
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-22 20:17:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -38,7 +38,6 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
+
 	return (0);
 }
