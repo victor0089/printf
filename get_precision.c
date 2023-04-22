0ac81f1fc083dcc2179e9fdb3ef729e2799270b5
Victor <victor.mecdoors@gmail.com>
2023-04-22 13:25:26 -0700

    fixing precision

--- get_precision.c
+++ get_precision.c
@@ -11,24 +11,24 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = -1;
+	int prc = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+		return (prc);
 
-	precision = 0;
+	prc = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			prc *= 10;
+			prc += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			precision = va_arg(list, int);
+			prc = va_arg(list, int);
 			break;
 		}
 		else
@@ -37,5 +37,5 @@ int get_precision(const char *format, int *i, va_list list)
 
 	*i = curr_i - 1;
 
-	return (precision);
+	return (prc);
 }
--- main.c
+++ main.c
@@ -39,6 +39,5 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
 	return (0);
 }
