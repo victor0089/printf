83a5d09935a7ca5923123cd5d9dd6af45c2c9344
NadaG20 <NadaMohamedGaber20@gmail.com>
2023-04-22 17:20:21 -0700

    Updating

--- func_0.c
+++ func_0.c
@@ -19,7 +19,7 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/*********************** PRINT A STRING ***********************/
+/********************* PRINT A STRING *********************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
