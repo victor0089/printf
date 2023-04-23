f10b6180054f66d62b089e21b946edd813372e67
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-24 00:13:18 +0200

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -11,7 +11,7 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	unsigned int curr_i = *i + 1;
-	unsigned int prc = -1;
+	unsigned int *prc = -1;
 
 	if (format[curr_i] != '.')
 		return (prc);
