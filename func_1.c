f61d1b9e86381c04c4f7c137cee1ddd3bb349f47
NadaG20 <NadaMohamedGaber20@gmail.com>
2023-04-22 17:17:11 -0700

    Updating

--- func_1.c
+++ func_1.c
@@ -113,7 +113,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		flags, 'X', width, precision, size));
 }
 
-/************ PRINT HEXX NUM IN LOWER OR UPPER ************/
+/*********** PRINT HEXX NUM IN LOWER OR UPPER ***********/
 /**
  * print_hexa - Prints a hexadecimal number in lower or upper
  * @types: Lista of arguments
