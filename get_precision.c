1fdd53f7e40c1256e818d43d8cb0e3dc8105a614
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-23 23:37:19 +0200

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -8,34 +8,20 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
-{
-	int curr_i = *i + 1;
-	int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+void get_precision(const char *format, ...) {
+    va_list list;
+    va_start(list, format);
+    
+    while (*format) {
+        if (*format == '%' && *(format+1) == 'p') {
+            int precision = va_arg(list, int);
+            double value = va_arg(list, double);
+            format += 2;
+        } else {
+            putchar(*format++);
+        }
+    }
+    
+    return(list);
 }
