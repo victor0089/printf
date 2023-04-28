8de7d75972d0f41b9b1cc3f4f6fe739f74919dcd
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-28 13:47:05 +0200

    Update and rename get_flags.c to geflag.c

--- geflag.c
+++ geflag.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * gflgs - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @y: take a parameter.
  *
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int gflgs(const char *format, int *y)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
-	int flags = 0;
+	int j, curr_y;
+	int flgs = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_y = *y + 1; format[curr_y] != '\0'; curr_y++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+			if (format[curr_y] == FLAGS_CH[j])
 			{
-				flags |= FLAGS_ARR[j];
+				flgs |= FLAGS_ARR[j];
 				break;
 			}
 
@@ -29,7 +29,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*y = curr_y - 1;
 
-	return (flags);
+	return (flgs);
 }
