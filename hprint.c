58ae8d62414df96ce596da18a351c3ab4cb29029
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-28 22:39:56 +0200

    Update hprint.c

--- hprint.c
+++ hprint.c
@@ -1,50 +1,83 @@
 #include "main.h"
-/**
- * hprint - Prints an argument based on its type
- * @victor: formatted string in which to print the arguments.
- * @lst: list of arguments to be printed.
- * @ind: ind.
- * @bufr: Buffer array is to handle print.
- * @flgs: calculates active flags
- * @wid: get width.
- * @prc: Precision specifications
- * @sz: Size specifier
- *
- * Return: 1 or 2;
- */
-int hprint(const char *victor, int *ind, va_list lst, char bufr[],
-	int flgs, int wid, int prc, int sz)
-{
-	int y, unknow_len = 0, pchars = -1;
-	victor_t victor_types[] = {
-		{'c', pchar}, {'s', pstring}, {'%', ppercent},
-		{'i', pint}, {'d', pint}, {'b', pbinary},
-		{'u', punsigned}, {'o', poctal}, {'x', phexadecimal},
-		{'X', phexa_upper}, {'p', ppointer}, {'S', pnon_printable},
-		{'r', preverse}, {'R', prot13string}, {'\0', NULL}
-	};
-	for (y = 0; victor_types[y].victor != '\0'; y++)
-		if (victor[*ind] == victor_types[y].victor)
-			return (victor_types[y].nad(lst, bufr, flgs, wid, prc, sz));
 
-	if (victor_types[y].victor == '\0')
-	{
-		if (victor[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%%", 1);
-		if (victor[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (wid)
-		{
-			--(*ind);
-			while (victor[*ind] != ' ' && victor[*ind] != '%')
-				--(*ind);
-			if (victor[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &victor[*ind], 1);
-		return (unknow_len);
-	}
-	return (pchars);
+/**
+* hprint - Prints an argument based on its type
+*
+* @victor: Formatted string in which to print the arguments.
+* @lst: List of arguments to be printed.
+* @ind: Index.
+* @bufr: Buffer array used to handle printing.
+* @flgs: Calculates active flags.
+* @wid: Width.
+* @prc: Precision specifications.
+* @sz: Size specifier.
+*
+* Return: The number of characters printed.
+*/
+int hprint(const char *victor, int *ind, va_list lst, char bufr[],
+int flgs, int wid, int prc, int sz)
+{
+int y, unknown_len = 0, pchars = -1;
+
+victor_t victor_types[] = {
+{'c', pchar},
+{'s', pstring},
+{'%', ppercent},
+{'i', pint},
+{'d', pint},
+{'b', pbinary},
+{'u', punsigned},
+{'o', poctal},
+{'x', phexadecimal},
+{'X', phexa_upper},
+{'p', ppointer},
+{'S', pnon_printable},
+{'r', preverse},
+{'R', prot13string},
+{'\0', NULL}
+};
+
+for (y = 0; victor_types[y].victor != '\0'; y++)
+{
+if (victor[*ind] == victor_types[y].victor)
+{
+return (victor_types[y].nad(lst, bufr, flgs, wid, prc, sz));
+}
+}
+
+if (victor_types[y].victor == '\0')
+{
+if (victor[*ind] == '\0')
+{
+return (-1);
+}
+
+unknown_len += write(1, "%%%", 1);
+
+if (victor[*ind - 1] == ' ')
+{
+unknown_len += write(1, " ", 1);
+}
+else if (wid)
+{
+--(*ind);
+
+while (victor[*ind] != ' ' && victor[*ind] != '%')
+{
+--(*ind);
+}
+
+if (victor[*ind] == ' ')
+{
+--(*ind);
+}
+
+return (1);
+}
+
+unknown_len += write(1, &victor[*ind], 1);
+return (unknown_len);
+}
+
+return (pchars);
 }
