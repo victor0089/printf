89f835829640bcf3ed9f8961bf8d8c29a2fae552
NadaG20 <NadaMohamedGaber20@gmail.com>
2023-04-28 11:33:50 -0700

    Updating

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * @c: Char is to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -16,8 +16,8 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * @buffer: array of chars.
+ * @i: index at which to start appending.
  * @ascii_code: ASSCI CODE.
  *
  * Return: Always 3
@@ -40,7 +40,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 /**
  * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * @c: Char is to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -54,8 +54,8 @@ int is_digit(char c)
 
 /**
  * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
+ * @num: number to be casted.
+ * @size: number indicating the type to be casted.
  *
  * Return: Casted value of num
  */
@@ -71,8 +71,8 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
+ * @num: number to be casted
+ * @size: number indicating the type to be casted
  *
  * Return: Casted value of num
  */
