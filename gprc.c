63abd632a3ac18e9628884ab8c9cc5b2193a8ee9
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-04-28 23:06:10 +0200

    Update gprc.c

--- gprc.c
+++ gprc.c
@@ -20,21 +20,21 @@ prc = 0;
 curr_y++;
 do
 {
-if (is_digit(format[curr_y])) 
+if (is_digit(format[curr_y]))
 {
 prc = (prc * 10) + (format[curr_y] - '0');
 curr_y++;
-} 
+}
 else if (format[curr_y] == '*')
 {
 curr_y++;
 prc = va_arg(lst, int);
-} 
+}
 else
 {
 break;
 }
-} 
+}
 while (1);
 
 *y = curr_y - 1;
